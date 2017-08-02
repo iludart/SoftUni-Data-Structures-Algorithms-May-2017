84ff2a8fb32cda11339111d9a4725abca35e3e1a
iludart <peter.penev@hotmail.com>
2017-08-02 10:17:39 +0300

    Combinatorial algorithms: snakes and cubes solutions

---
+++ C#/Algorithms/03. Combinatorial-Algorithms/Cubes.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Cubes
+{
+    static int count = 0;
+    static HashSet<string> marked = new HashSet<string>();
+
+    static void Main(string[] args)
+    {
+        var cube = Console.ReadLine()
+            .Split()
+            .Select(x => x[0])
+            .ToArray();
+
+        Permute(cube, 0);
+        
+        Console.WriteLine(count);
+    }
+
+    private static void Permute(char[] cube, int index)
+    {
+        if (index >= cube.Length)
+        {
+            if (!marked.Contains(new string(cube)))
+            {
+                MarkCube(cube);
+                count++;
+            }
+        }
+        else
+        {
+            Permute(cube, index + 1);
+            HashSet<char> used = new HashSet<char>();
+            used.Add(cube[index]);
+            for (int i = index + 1; i < cube.Length; i++)
+            {
+                if (!used.Contains(cube[i]))
+                {
+                    Swap(cube, index, i);
+                    Permute(cube, index + 1);
+                    Swap(cube, index, i);
+
+                    used.Add(cube[i]);
+                }
+            }
+        }
+    }
+
+    private static void MarkCube(char[] cube)
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                for (int z = 0; z < 4; z++)
+                {
+                    marked.Add(new string(cube));
+                    cube = RotateZ(cube);
+                }
+                cube = RotateY(cube);
+            }
+            cube = RotateX(cube);
+        }
+    }
+
+    private static char[] RotateZ(char[] cube) // ok
+    {
+        var rotated = new char[cube.Length];
+        rotated[0] = cube[1];
+        rotated[1] = cube[2];
+        rotated[2] = cube[3];
+        rotated[3] = cube[0];
+
+        rotated[4] = cube[5];
+        rotated[5] = cube[6];
+        rotated[6] = cube[7];
+        rotated[7] = cube[4];
+
+        rotated[8] = cube[9];
+        rotated[9] = cube[10];
+        rotated[10] = cube[11];
+        rotated[11] = cube[8];
+
+        return rotated;
+    }
+
+    private static char[] RotateX(char[] cube)
+    {
+        var rotated = new char[cube.Length];
+        rotated[0] = cube[9];
+        rotated[1] = cube[5];
+        rotated[2] = cube[10];
+        rotated[3] = cube[1];
+
+        rotated[4] = cube[8];
+        rotated[5] = cube[7];
+        rotated[6] = cube[11];
+        rotated[7] = cube[3];
+
+        rotated[8] = cube[0];
+        rotated[9] = cube[4];
+        rotated[10] = cube[6];
+        rotated[11] = cube[2];
+
+        return rotated;
+    }
+
+    private static char[] RotateY(char[] cube)
+    {
+        var rotated = new char[cube.Length];
+        rotated[0] = cube[2];
+        rotated[1] = cube[10];
+        rotated[2] = cube[6];
+        rotated[3] = cube[11];
+
+        rotated[4] = cube[0];
+        rotated[5] = cube[9];
+        rotated[6] = cube[4];
+        rotated[7] = cube[8];
+
+        rotated[8] = cube[3];
+        rotated[9] = cube[1];
+        rotated[10] = cube[5];
+        rotated[11] = cube[7];
+
+        return rotated;
+    }
+
+    private static void Swap(char[] cube, int i, int j)
+    {
+        var temp = cube[i];
+        cube[i] = cube[j];
+        cube[j] = temp;
+    }
+}
---
+++ C#/Algorithms/03. Combinatorial-Algorithms/Snakes.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Snakes
+{
+    static int count = 0;
+    static HashSet<string> snakes = new HashSet<string>();
+    static HashSet<string> visited = new HashSet<string>();
+
+    static char[] snake = { 'S', 'R', 'U' };
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        snake = new char[n];
+        snake[0] = 'S';
+        visited.Add(0 + " " + 0);
+
+        Generate(1, 0, 1, 'R');
+        Console.WriteLine("Snakes count = " + count);
+    }
+
+    private static void Generate(int index, int row, int col, char direction)
+    {
+        if (index >= snake.Length)
+        {
+            TryAddSnake();
+        }
+        else
+        {
+            var cell = row + " " + col;
+            if (!visited.Contains(cell))
+            {
+                visited.Add(cell);
+                snake[index] = direction;
+
+                Generate(index + 1, row, col + 1, 'R');
+                Generate(index + 1, row + 1, col, 'D');
+                Generate(index + 1, row, col - 1, 'L');
+                Generate(index + 1, row - 1, col, 'U');
+
+                visited.Remove(cell);
+            }
+        }
+    }
+
+    private static void TryAddSnake()
+    {
+        var normal = new string(snake);
+        var flipped = FlipY(normal);
+        var reversed = Reversed();
+
+        if (snakes.Contains(normal) || snakes.Contains(flipped) || snakes.Contains(reversed))
+        {
+            return;
+        }
+        
+        snakes.Add(Rotate(reversed.ToCharArray()));
+        snakes.Add(Rotate(flipped.ToCharArray()));
+
+        Console.WriteLine(normal);
+        count++;
+    }
+
+    private static string Reversed()
+    {
+        var reversed = new char[snake.Length];
+        reversed[0] = 'S';
+        for (int i = snake.Length - 1; i > 0; i--)
+        {
+            reversed[snake.Length - i] = snake[i];
+        }
+
+        return new string(reversed);
+    }
+
+    private static string FlipY(string toFlip)
+    {
+        var flipped = new char[toFlip.Length];
+        for (int i = 0; i < toFlip.Length; i++)
+        {
+            if (toFlip[i] == 'D')
+            {
+                flipped[i] = 'U';
+            }
+            else if (toFlip[i] == 'U')
+            {
+                flipped[i] = 'D';
+            }
+            else
+            {
+                flipped[i] = toFlip[i];
+            }
+        }
+
+        return new string(flipped);
+    }
+
+    private static string Rotate(char[] toRotate)
+    {
+        while (toRotate[1] != 'R')
+        {
+            for (int i = 1; i < toRotate.Length; i++)
+            {
+                switch (toRotate[i])
+                {
+                    case 'R': toRotate[i] = 'D'; break;
+                    case 'D': toRotate[i] = 'L'; break;
+                    case 'L': toRotate[i] = 'U'; break;
+                    case 'U': toRotate[i] = 'R'; break;
+                    default: break;
+                }
+            }
+        }
+
+        return new string(toRotate);
+    }
+}
